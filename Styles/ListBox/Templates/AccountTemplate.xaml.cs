6f7a0a8257745841028b7642ee4c46e94ff48714
kovtun_dv <kovtun_dv@groupbwt.com>
2020-02-01 17:46:48 +0200

    add 'AccountDataTemplate'

---
+++ Styles/ListBox/Templates/AccountTemplate.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace UI4All.Styles.ListBox.Templates
+{
+	/// <summary>
+	/// Interaction logic for AccountTemplate.xaml
+	/// </summary>
+	public partial class AccountTemplate : UserControl, INotifyPropertyChanged
+	{
+		public AccountTemplate()
+		{
+			InitializeComponent();
+		}
+
+		#region INotifyPropertyChanged implementation
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+
+		#endregion
+	}
+}
