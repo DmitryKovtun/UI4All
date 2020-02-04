298b274bc43e0e1dcad8d65f06cdb66d8f905065
DmitryKovtun <dm_ko@outlook.com>
2020-02-04 20:36:01 +0200

    rename file

---
+++ Styles/ListBox/AccountListBoxControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace UI4All.Styles.ListBox
+{
+	/// <summary>
+	/// Interaction logic for AccountTagListBox.xaml
+	/// </summary>
+	public partial class AccountListBoxControl : UserControl
+	{
+		public AccountListBoxControl()
+		{
+			InitializeComponent();
+			//DataContext = this;
+			//Main.DataContext = DataContext;
+			//DataContext
+		}
+
+
+		public IEnumerable ItemsSource
+		{
+			get { return Main.ItemsSource; }
+			set { Main.ItemsSource = value; }
+		}
+
+
+
+
+		private ICommand _ItemCLickCommand;
+
+		public virtual ICommand ItemCLickCommand
+		{
+			get
+			{
+				if(_ItemCLickCommand == null)
+					_ItemCLickCommand = new DelegateCommand(delegate ()
+					{
+
+						MessageBox.Show("Texccxcxcxcxxccxcxcxst");
+					});
+				return _ItemCLickCommand;
+			}
+		}
+	}
+}
